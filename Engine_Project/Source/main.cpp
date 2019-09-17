892ba6c8569ce32bce33b94e1a85a8144711469b
Runwu1997 <36273424+Runwu1997@users.noreply.github.com>
2019-09-17 14:33:29 -0400

    Implemented Storage check

--- Engine_Project/Source/main.cpp
+++ Engine_Project/Source/main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
+#include <Windows.h>
+#include <direct.h>
+#include <stdio.h>
+#include <tchar.h>
+const DWORDLONG storageNeed = 300;
+const DWORDLONG pRAMNeed = 1;
+const DWORDLONG vRAMNeed = 1;
 
-int main() 
+using namespace std;
+
+bool CheckStorage(const DWORDLONG diskSpaceNeeded);
+
+
+int main()
 {
-	std::cout << "Hello World" << std::endl;
+	bool storageCheck;
+	storageCheck = CheckStorage(storageNeed);
+	if (storageCheck == true)
+	{
+		cout << "You hava enough storage!" << endl;
+	}
 
 	return 0;
 }
+
+bool CheckStorage(const DWORDLONG diskSpaceNeeded)
+{
+	int const drive = _getdrive();
+	struct _diskfree_t diskfree;
+	_getdiskfree(drive, &diskfree);
+	unsigned __int64 neededClusters;
+	neededClusters = diskSpaceNeeded /
+		(diskfree.sectors_per_cluster *
+			diskfree.bytes_per_sector);
+	if (diskfree.avail_clusters < neededClusters)
+	{
+		cout << "CheckStorage Failure: Not enough physical storage." << endl;
+		return false;
+
+	}
+	return true;
+}
+
