16de9f570253e5d894e6eeea7a0f9e74f2d2e558
Xian Zhang <39228340+Xian767-6424@users.noreply.github.com>
2019-09-19 08:57:16 -0400

    lab2
    lab2

--- Engine_Project/Source/main.cpp
+++ Engine_Project/Source/main.cpp
@@ -11,37 +11,37 @@ typedef ULONGLONG DWORDLONG, *PDWORDLONG;
 
 using namespace std;
 
-//extern bool CheckStorage(const DWORDLONG diskSpaceNeeded);
+extern bool CheckStorage(const DWORDLONG diskSpaceNeeded);
 extern DWORD ReadCPUSpeed();
-//extern bool CheckMemory(const DWORDLONG physicalRAMNeeded, const DWORDLONG virtualRAMNeeded);
-//extern bool IsOnlyInstance(const TCHAR* gameTitle);
+extern bool CheckMemory(const DWORDLONG physicalRAMNeeded, const DWORDLONG virtualRAMNeeded);
+extern bool IsOnlyInstance(const TCHAR* gameTitle);
 
 int main()
 {
 	//std::cout << "Hello World" << std::endl;
 	ReadCPUSpeed();
-	//if (CheckStorage(307200))//can pass the function to if() directly
-	//{
-	//	std::cout << "You have 300 MB available storage\n";
-	//}
-	//else
-	//{
-	//	std::cout << "No enought space to install the game\n";
-	//}
-	//if (!IsOnlyInstance)
-	//{
-	//	std::cout << "Hey! don't try to cheat on the game\n";
-	//}
+	if (CheckStorage(307200))//can pass the function to if() directly
+	{
+		std::cout << "You have 300 MB available storage\n";
+	}
+	else
+	{
+		std::cout << "No enought space to install the game\n";
+	}
+	if (!IsOnlyInstance)
+	{
+		std::cout << "Hey! don't try to cheat on the game\n";
+	}
 
-	//if (CheckMemory(2e+6, 2e+6))
-	//{
-	//	std::cout << "You have enough memory\n";
-	//}
-	//else
-	//{
-	//	std::cout << "You don't have enough memory\n";
-	//}
-	//return 0;
+	if (CheckMemory(2e+6, 2e+6))
+	{
+		std::cout << "You have enough memory\n";
+	}
+	else
+	{
+		std::cout << "You don't have enough memory\n";
+	}
+	return 0;
 }
 DWORD ReadCPUSpeed()
 {
@@ -67,90 +67,90 @@ DWORD ReadCPUSpeed()
 }
 // CheckMemory
 //
-//bool CheckMemory(const DWORDLONG physicalRAMNeeded, const DWORDLONG virtualRAMNeeded)
-//{
-//	MEMORYSTATUSEX status;
-//	GlobalMemoryStatusEx(&status);
-//	if (status.ullTotalPhys < physicalRAMNeeded)
-//	{
-//		// you donít have enough physical memory. Tell the player to go get a real 
-//		// computer and give this one to his mother. 
-//		std::cout << "CheckMemory Failure: Not enough physical memory." << std::endl;
-//		return false;
-//	}
-//	else {
-//		std::cout << "physical memory:" << status.ullTotalPhys << "\n" << std::endl;
-//	}
-//
-//	// Check for enough free memory.
-//	if (status.ullAvailVirtual < virtualRAMNeeded)
-//	{
-//		// you donít have enough virtual memory available. 
-//		// Tell the player to shut down the copy of Visual Studio running in the
-//		// background, or whatever seems to be sucking the memory dry.
-//		std::cout << "CheckMemory Failure: Not enough virtual memory." << std::endl;
-//		return false;
-//	}
-//	else {
-//		std::cout << "virtual memory:" << status.ullAvailVirtual << "\n" << std::endl;
-//	}
-//
-//	char* buff = new char[(unsigned int)virtualRAMNeeded];
-//	if (buff)
-//		delete[] buff;
-//	else
-//	{
-//		// even though there is enough memory, it isnít available in one 
-//		// block, which can be critical for games that manage their own memory
-//		std::cout << "CheckMemory Failure: Not enough contiguous available memory." << std::endl;
-//		return false;
-//	}
-//	return true;
-//}
+bool CheckMemory(const DWORDLONG physicalRAMNeeded, const DWORDLONG virtualRAMNeeded)
+{
+	MEMORYSTATUSEX status;
+	GlobalMemoryStatusEx(&status);
+	if (status.ullTotalPhys < physicalRAMNeeded)
+	{
+		// you donít have enough physical memory. Tell the player to go get a real 
+		// computer and give this one to his mother. 
+		std::cout << "CheckMemory Failure: Not enough physical memory." << std::endl;
+		return false;
+	}
+	else {
+		std::cout << "physical memory:" << status.ullTotalPhys << "\n" << std::endl;
+	}
 
-//bool CheckStorage(const DWORDLONG diskSpaceNeeded)
-//{
-//	// Check for enough free disk space on the current disk.
-//	int const drive = _getdrive();
-//	struct _diskfree_t diskfree = { 0 };
-//
-//	_getdiskfree(drive, &diskfree);
-//
-//	unsigned __int64 const neededClusters =
-//		diskSpaceNeeded / (diskfree.sectors_per_cluster * diskfree.bytes_per_sector);
-//
-//	if (diskfree.avail_clusters < neededClusters)
-//	{
-//		// if you get here you donít have enough disk space!
-//		std::cout << "CheckStorage Failure: Not enough physical storage." << std::endl;
-//		return false;
-//	}
-//	return true;
-//}
+	// Check for enough free memory.
+	if (status.ullAvailVirtual < virtualRAMNeeded)
+	{
+		// you donít have enough virtual memory available. 
+		// Tell the player to shut down the copy of Visual Studio running in the
+		// background, or whatever seems to be sucking the memory dry.
+		std::cout << "CheckMemory Failure: Not enough virtual memory." << std::endl;
+		return false;
+	}
+	else {
+		std::cout << "virtual memory:" << status.ullAvailVirtual << "\n" << std::endl;
+	}
 
+	char* buff = new char[(unsigned int)virtualRAMNeeded];
+	if (buff)
+		delete[] buff;
+	else
+	{
+		// even though there is enough memory, it isnít available in one 
+		// block, which can be critical for games that manage their own memory
+		std::cout << "CheckMemory Failure: Not enough contiguous available memory." << std::endl;
+		return false;
+	}
+	return true;
+}
 
+bool CheckStorage(const DWORDLONG diskSpaceNeeded)
+{
+	// Check for enough free disk space on the current disk.
+	int const drive = _getdrive();
+	struct _diskfree_t diskfree = { 0 };
+
+	_getdiskfree(drive, &diskfree);
+
+	unsigned __int64 const neededClusters =
+		diskSpaceNeeded / (diskfree.sectors_per_cluster * diskfree.bytes_per_sector);
+
+	if (diskfree.avail_clusters < neededClusters)
+	{
+		// if you get here you donít have enough disk space!
+		std::cout << "CheckStorage Failure: Not enough physical storage." << std::endl;
+		return false;
+	}
+	return true;
+}
 
 
-//bool IsOnlyInstance(const TCHAR* gameTitle)
-//{
-//	// Find the window.  If active, set and return false
-//	// Only one game instance may have this mutex at a time...
-//
-//	HANDLE handle = CreateMutex(NULL, TRUE, gameTitle);
-//
-//	// Does anyone else think 'ERROR_SUCCESS' is a bit of an oxymoron?
-//	if (GetLastError() != ERROR_SUCCESS)
-//	{
-//		HWND hWnd = FindWindow(gameTitle, NULL);
-//		if (hWnd)
-//		{
-//			// An instance of your game is already running.
-//			ShowWindow(hWnd, SW_SHOWNORMAL);
-//			SetFocus(hWnd);
-//			SetForegroundWindow(hWnd);
-//			SetActiveWindow(hWnd);
-//			return false;
-//		}
-//	}
-//	return true;
-//}
+
+
+bool IsOnlyInstance(const TCHAR* gameTitle)
+{
+	// Find the window.  If active, set and return false
+	// Only one game instance may have this mutex at a time...
+
+	HANDLE handle = CreateMutex(NULL, TRUE, gameTitle);
+
+	// Does anyone else think 'ERROR_SUCCESS' is a bit of an oxymoron?
+	if (GetLastError() != ERROR_SUCCESS)
+	{
+		HWND hWnd = FindWindow(gameTitle, NULL);
+		if (hWnd)
+		{
+			// An instance of your game is already running.
+			ShowWindow(hWnd, SW_SHOWNORMAL);
+			SetFocus(hWnd);
+			SetForegroundWindow(hWnd);
+			SetActiveWindow(hWnd);
+			return false;
+		}
+	}
+	return true;
+}
