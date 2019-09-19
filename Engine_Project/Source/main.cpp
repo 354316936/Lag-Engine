67eebc8d94eb5457ce5096987f2ea333add3b9de
Xian Zhang <39228340+Xian767-6424@users.noreply.github.com>
2019-09-19 08:25:27 -0400

    Check Memory
    Check Memory function

--- Engine_Project/Source/main.cpp
+++ Engine_Project/Source/main.cpp
@@ -5,33 +5,33 @@
 #include <direct.h>
 
 
-typedef unsigned long DWORD, * PDWORD, * LPDWORD;
+//typedef unsigned long DWORD, * PDWORD, * LPDWORD;
 typedef ULONGLONG DWORDLONG, * PDWORDLONG;
 
 
 using namespace std;
 
-extern bool CheckStorage(const DWORDLONG diskSpaceNeeded);
-extern DWORD ReadCPUSpeed();
+//extern bool CheckStorage(const DWORDLONG diskSpaceNeeded);
+//extern DWORD ReadCPUSpeed();
 extern bool CheckMemory(const DWORDLONG physicalRAMNeeded, const DWORDLONG virtualRAMNeeded);
-extern bool IsOnlyInstance(const TCHAR* gameTitle);
+//extern bool IsOnlyInstance(const TCHAR* gameTitle);
 
 int main() 
 {
 	//std::cout << "Hello World" << std::endl;
-	ReadCPUSpeed();
-	if (CheckStorage(307200))//can pass the function to if() directly
-	{
-		std::cout << "You have 300 MB available storage\n";
-	}
-	else
-	{
-		std::cout << "No enought space to install the game\n";
-	}
-	if (!IsOnlyInstance)
-	{
-		std::cout << "Hey! don't try to cheat on the game\n";
-	}
+	//ReadCPUSpeed();
+	//if (CheckStorage(307200))//can pass the function to if() directly
+	//{
+	//	std::cout << "You have 300 MB available storage\n";
+	//}
+	//else
+	//{
+	//	std::cout << "No enought space to install the game\n";
+	//}
+	//if (!IsOnlyInstance)
+	//{
+	//	std::cout << "Hey! don't try to cheat on the game\n";
+	//}
 
 	if (CheckMemory(2e+6, 2e+6))
 	{
@@ -42,48 +42,6 @@ int main()
 		std::cout << "You don't have enough memory\n";
 	}
 	return 0;
-}
-
-bool CheckStorage(const DWORDLONG diskSpaceNeeded)
-{
-	// Check for enough free disk space on the current disk.
-	int const drive = _getdrive();
-	struct _diskfree_t diskfree = { 0 };
-
-	_getdiskfree(drive, &diskfree);
-
-	unsigned __int64 const neededClusters =
-		diskSpaceNeeded / (diskfree.sectors_per_cluster * diskfree.bytes_per_sector);
-
-	if (diskfree.avail_clusters < neededClusters)
-	{
-		// if you get here you donít have enough disk space!
-		std::cout << "CheckStorage Failure: Not enough physical storage." << std::endl;
-		return false;
-	}
-	return true;
-}
-DWORD ReadCPUSpeed()
-{
-	DWORD BufSize = sizeof(DWORD);
-	DWORD dwMHz = 0;
-	DWORD type = REG_DWORD;
-	HKEY hKey;
-	// open the key where the proc speed is hidden:
-	long lError = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
-		L"HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0",
-		0, KEY_READ, &hKey);
-
-	if (lError == ERROR_SUCCESS)
-	{
-		// query the key:
-		RegQueryValueEx(hKey, L"~MHz", NULL, &type, (LPBYTE)& dwMHz, &BufSize);
-		std::cout <<"MHz: " <<dwMHz<<"\n" << std::endl;
-		std::cout << "hKey: " << hKey << "\n" << std::endl;
-		std::cout << "type: " << type << "\n" << std::endl;
-		std::cout << "BufSize: " << BufSize << "\n" << std::endl;
-	}
-	return dwMHz;
 }
 
 // CheckMemory
@@ -100,7 +58,7 @@ bool CheckMemory(const DWORDLONG physicalRAMNeeded, const DWORDLONG virtualRAMNe
 		return false;
 	}
 	else {
-		std::cout << "physical memory:"<< status.ullTotalPhys<< "\n" << std::endl;
+		std::cout << "physical memory:" << status.ullTotalPhys << "\n" << std::endl;
 	}
 
 	// Check for enough free memory.
@@ -129,26 +87,70 @@ bool CheckMemory(const DWORDLONG physicalRAMNeeded, const DWORDLONG virtualRAMNe
 	return true;
 }
 
-bool IsOnlyInstance(const TCHAR* gameTitle)
-{
-	// Find the window.  If active, set and return false
-	// Only one game instance may have this mutex at a time...
+//bool CheckStorage(const DWORDLONG diskSpaceNeeded)
+//{
+//	// Check for enough free disk space on the current disk.
+//	int const drive = _getdrive();
+//	struct _diskfree_t diskfree = { 0 };
+//
+//	_getdiskfree(drive, &diskfree);
+//
+//	unsigned __int64 const neededClusters =
+//		diskSpaceNeeded / (diskfree.sectors_per_cluster * diskfree.bytes_per_sector);
+//
+//	if (diskfree.avail_clusters < neededClusters)
+//	{
+//		// if you get here you donít have enough disk space!
+//		std::cout << "CheckStorage Failure: Not enough physical storage." << std::endl;
+//		return false;
+//	}
+//	return true;
+//}
+//DWORD ReadCPUSpeed()
+//{
+//	DWORD BufSize = sizeof(DWORD);
+//	DWORD dwMHz = 0;
+//	DWORD type = REG_DWORD;
+//	HKEY hKey;
+//	// open the key where the proc speed is hidden:
+//	long lError = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+//		L"HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0",
+//		0, KEY_READ, &hKey);
+//
+//	if (lError == ERROR_SUCCESS)
+//	{
+//		// query the key:
+//		RegQueryValueEx(hKey, L"~MHz", NULL, &type, (LPBYTE)& dwMHz, &BufSize);
+//		std::cout <<"MHz: " <<dwMHz<<"\n" << std::endl;
+//		std::cout << "hKey: " << hKey << "\n" << std::endl;
+//		std::cout << "type: " << type << "\n" << std::endl;
+//		std::cout << "BufSize: " << BufSize << "\n" << std::endl;
+//	}
+//	return dwMHz;
+//}
 
-	HANDLE handle = CreateMutex(NULL, TRUE, gameTitle);
 
-	// Does anyone else think 'ERROR_SUCCESS' is a bit of an oxymoron?
-	if (GetLastError() != ERROR_SUCCESS)
-	{
-		HWND hWnd = FindWindow(gameTitle, NULL);
-		if (hWnd)
-		{
-			// An instance of your game is already running.
-			ShowWindow(hWnd, SW_SHOWNORMAL);
-			SetFocus(hWnd);
-			SetForegroundWindow(hWnd);
-			SetActiveWindow(hWnd);
-			return false;
-		}
-	}
-	return true;
-}
+
+//bool IsOnlyInstance(const TCHAR* gameTitle)
+//{
+//	// Find the window.  If active, set and return false
+//	// Only one game instance may have this mutex at a time...
+//
+//	HANDLE handle = CreateMutex(NULL, TRUE, gameTitle);
+//
+//	// Does anyone else think 'ERROR_SUCCESS' is a bit of an oxymoron?
+//	if (GetLastError() != ERROR_SUCCESS)
+//	{
+//		HWND hWnd = FindWindow(gameTitle, NULL);
+//		if (hWnd)
+//		{
+//			// An instance of your game is already running.
+//			ShowWindow(hWnd, SW_SHOWNORMAL);
+//			SetFocus(hWnd);
+//			SetForegroundWindow(hWnd);
+//			SetActiveWindow(hWnd);
+//			return false;
+//		}
+//	}
+//	return true;
+//}
