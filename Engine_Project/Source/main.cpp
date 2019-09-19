edc50ce8b6dd3f3f736503d786d5341a7256d6d7
Xian Zhang <39228340+Xian767-6424@users.noreply.github.com>
2019-09-19 09:04:27 -0400

    Nothing change here
    Just merge by hands

--- Engine_Project/Source/main.cpp
+++ Engine_Project/Source/main.cpp
@@ -8,6 +8,13 @@
 typedef unsigned long DWORD, * PDWORD, * LPDWORD;
 typedef ULONGLONG DWORDLONG, *PDWORDLONG;
 
+const DWORDLONG storageNeed = 300;
+const DWORDLONG pRAMNeed = 1;
+const DWORDLONG vRAMNeed = 1;
+
+using namespace std;
+bool IsOnlyInstance(LPCTSTR gameTitle);
+bool CheckStorage(const DWORDLONG diskSpaceNeeded);
 
 using namespace std;
 
@@ -42,6 +49,60 @@ int main()
 		std::cout << "You don't have enough memory\n";
 	}
 	return 0;
+}
+
+int main()
+{
+	bool storageCheck;
+	storageCheck = CheckStorage(storageNeed);
+	if (storageCheck == true)
+	{
+		cout << "You hava enough storage!" << endl;
+	}
+
+	bool instanceCheck;
+	LPCTSTR GT = "Engine";
+	instanceCheck = IsOnlyInstance(GT);
+	if (instanceCheck = 1)
+	{
+		cout << "The game instance check success!" << endl;
+	}
+	return 0;
+}
+
+
+bool IsOnlyInstance(LPCTSTR gameTitle)
+{
+	HANDLE handle = CreateMutex(NULL, TRUE, gameTitle);
+	if (GetLastError() != ERROR_SUCCESS) {
+		HWND hWnd = FindWindow(gameTitle, NULL);
+		if (hWnd) {
+			ShowWindow(hWnd, SW_SHOWNORMAL);
+			SetFocus(hWnd);
+			SetForegroundWindow(hWnd);
+			SetActiveWindow(hWnd);
+			return false;
+		}
+	}
+	return true;
+}
+
+bool CheckStorage(const DWORDLONG diskSpaceNeeded)
+{
+	int const drive = _getdrive();
+	struct _diskfree_t diskfree;
+	_getdiskfree(drive, &diskfree);
+	unsigned __int64 neededClusters;
+	neededClusters = diskSpaceNeeded /
+		(diskfree.sectors_per_cluster *
+			diskfree.bytes_per_sector);
+	if (diskfree.avail_clusters < neededClusters)
+	{
+		cout << "CheckStorage Failure: Not enough physical storage." << endl;
+		return false;
+
+	}
+	return true;
 }
 DWORD ReadCPUSpeed()
 {
